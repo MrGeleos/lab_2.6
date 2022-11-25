55e8b09d4410efebbc0170436c3e4fe73828db81
MrGeleos <mr.geleos12@mail.ru>
2022-11-25 22:46:28 +0300

    add programm

--- individual.py
+++ individual.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from datetime import date
-
 
 if __name__ == '__main__':
     trains = []
+    print("Список команд:\n")
+    print("add - добавить студента;")
+    print("list - вывести список студентов;")
+    print("select - запросить студентов с баллом выше 4.0;")
+    print("exit - завершить работу с программой.")
 
     while True:
         command = input(">>> ").lower()
@@ -15,9 +18,9 @@ if __name__ == '__main__':
             break
 
         elif command == 'add':
-            destination = input("Пункт назначения?")
-            number = input("Номер поезда?")
-            time = input("Время отправления?")
+            destination = input("Пункт назначения? ")
+            number = input("Номер поезда? ")
+            time = input("Время отправления (ЧЧ:ММ)? ")
 
             train = {
                 'destination': destination,
@@ -30,15 +33,15 @@ if __name__ == '__main__':
                 trains.sort(key=lambda item: item.get('time', ''))
 
         elif command == 'list':
-            line = '+-{}-+-{}-+-{}-+-{}-+'.format(
+            line = "+-{}-+-{}-+-{}-+-{}-+".format(
                 '-' * 4,
                 '-' * 30,
                 '-' * 20,
-                '-' * 8
+                '-' * 20
             )
             print(line)
             print(
-                '| {:^4} | {:^30} | {:^20} | {:^8} |'.format(
+                '| {:^4} | {:^30} | {:^20} | {:^20} |'.format(
                     "№",
                     "Пункт",
                     "Номер поезда",
@@ -49,12 +52,32 @@ if __name__ == '__main__':
 
             for idx, train in enumerate(trains, 1):
                 print(
-                    '| {:>4} | {:<30} | {:<20} | {:>8} |'.format(
+                    '| {:>4} | {:<30} | {:<20} | {:>20} |'.format(
                         idx,
                         train.get('destination', ''),
                         train.get('number', ''),
                         train.get('time', 0)
                     )
                 )
-
             print(line)
+
+        elif command.startswith('select'):
+
+            destination1 = input('Введите название пункта: ')
+
+            count = 0
+
+            for train in trains:
+                if train.get('destination', '') == destination1:
+                    print(
+                        '{:>1} {}'.format('Пункт: ', train.get('destination', '')),
+                        '{:>1} {}'.format('Номер поезда: ', train.get('number', '')),
+                        '{:>1} {}'.format('Время отправления: ', train.get('time', 0))
+                    )
+                    count += 1
+
+            if count == 0:
+                print("Поезд с таким пунктом не найден.")
+
+        else:
+            print(f"Неизвестная команда {command}", file=sys.stderr)
